b4558335100b5571b6a4ddd35a9036839b804d7c
MissZaraMusic <125260653+MissZaraMusic@users.noreply.github.com>
2023-02-14 10:05:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/431375211abb40e7bace3.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
