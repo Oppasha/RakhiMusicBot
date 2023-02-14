628b87a0e8c3ddc0ce3410cee1016baf54fc9105
MissZaraMusic <125260653+MissZaraMusic@users.noreply.github.com>
2023-02-14 10:31:54 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("")
+            await message.reply_sticker("CAACAgUAAxkDAAICjWPrDqjQTs1GMepfRGPWzGd4SdILAAI7CAACmoKJV_jtFeuLb8yvLgQ")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
